f1990dbb45fedcfcb0b946eef4c05f3365b63e93
maion308 <michael.ion.mi@outlook.com>
2018-06-26 14:23:50 -0400

    nunjucks files added

---
+++ includes/express.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var path = require('path');
+const app = express();
+var nunjucks = require('nunjucks');
+var bodyParser = require('body-parser');
+
+nunjucks.configure('templates', {
+ autoescape: true,
+ express: app 
+});
+
+app.set('view engine','nunjucks');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+ extended: false
+}));
+
+app.use(express.static('includes'));
+
+
+
+app.route('/')
+ .get((req, res) => {
+   res.render('index');
+})
+
+app.route('/index')
+ .get((req, res) => {
+   res.render('index');
+})
+
+app.route('/contact-us')
+ .get((req, res) => {
+   res.render('contact-us');
+})
+
+app.route('/inventory')
+ .get((req, res) => {
+   res.render('inventory');
+})
+
+app.listen(9000);
